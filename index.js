5cefcf83b8f103c5310b66407128a7552663a670
AkhiSharmin <akhisharmin100@gmail.com>
2023-05-13 03:03:33 +0600

    updated

--- index.js
+++ index.js
@@ -43,7 +43,8 @@ async function run() {
         app.get('/coffee/:id', async (req, res) => {
             const id = req.params.id;
             const query = { _id: new ObjectId(id) }
-
+            const result = await coffeeCollection.findOne(query);
+            res.send(result)
         })
 
 
