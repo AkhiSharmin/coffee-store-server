417081951e2a1cfd2a463e49f4a43da56f78f2d6
AkhiSharmin <akhisharmin100@gmail.com>
2023-05-14 00:58:33 +0600

    updated cofeee

--- index.js
+++ index.js
@@ -57,6 +57,25 @@ async function run() {
         })
 
 
+        app.put('/coffee/:id', async (req, res) => {
+            const id = req.params.id;
+            const filter = { _id: new ObjectId(id) }
+            const options = { upsert: true }
+            const updatedCoffee = {
+                $set: {
+                    name: updatedCoffee.name,
+                    quantity: updatedCoffee.quantity,
+                    supplier: updatedCoffee.supplier,
+                    taste: updatedCoffee.taste,
+                    category: updatedCoffee.category,
+                    details: updatedCoffee.details,
+                    photo: updatedCoffee.photo
+                }
+            }
+        })
+
+
+
         app.delete('/coffee/:id', async (req, res) => {
             const id = req.params.id;
             const query = { _id: new ObjectId(id) };
